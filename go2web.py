3fd1f4c8f46fa3de5699e877dcf8dec63e09b0c9
buffaloss <beatricia.golban@isa.utm.md>
2024-03-30 12:01:54 +0200

    Add HTTP request function

--- go2web.py
+++ go2web.py
@@ -1,4 +1,5 @@
 import sys
+import socket
 
 def show_help():
     print("\nUsage: go2web [options]")
@@ -7,6 +8,30 @@ def show_help():
     print("  -s <search-term>    Make an HTTP request to search a term using a search engine and print the top 10 results")
     print("  -h                  Show help")
     print()
+
+import socket
+
+import socket
+
+def http_request(url):
+    url_parts = url.split('/')
+    target_host = url_parts[2]
+    target_path = '/' + '/'.join(url_parts[3:])
+    
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.connect((target_host, 80))
+    http_request_msg = f"GET {target_path} HTTP/1.1\r\nHost: {target_host}\r\n\r\n"
+    connection.send(http_request_msg.encode())
+    http_response = b''
+    while True:
+        data = connection.recv(1024)
+        if not data:
+            break
+        http_response += data
+    
+    connection.close()
+    
+    print(http_response.decode())
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or sys.argv[1] != '-h':
